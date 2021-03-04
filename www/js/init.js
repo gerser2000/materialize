792cb18dac5ff25a0a0980b1327270fd446498c1
unknown <nomengerard@gmail.com>
2021-03-04 20:09:04 +0100

    lista bien

--- www/js/init.js
+++ www/js/init.js
@@ -18,13 +18,34 @@
   }); // end of document ready
 })(jQuery); // end of jQuery name space
  
+var bcerca = document.getElementById("bcerca");
+
+
+
+function buscar(){
+    var cerca = document.getElementById("cerca").value;
+    console.log("hola");
+    $.ajax({
+        method: "GET",
+        url: "https://musicbrainz.org/ws/2/artist?query="+cerca,
+        dataType: "json",   
+      }).done(function (msg) {
+        for(var item in msg.artists) {
+          console.log(msg.artists[item].name);
+          $("#artlistas").append('<li class="collection-item black"><p class="black" class="white-text">'+msg.artists[item].name+'</p></li><br/><br/>')
+        };
+      }).fail(function () {
+          alert("ERROR");
+      });
+}
 
 document.addEventListener('deviceready', onDeviceReady, false);
  
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
- 
+
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById("bcerca").onclick = buscar;
     //document.getElementById('deviceready').classList.add('ready');
 }
 
